e7e631c5c12ad188957ddef15663a5694a731b87
Amir Maghami <Maghami.A1987@gmail.com>
2026-01-04 19:34:52 +0330

    Update tag.entity.ts

--- src/tags/entities/tag.entity.ts
+++ src/tags/entities/tag.entity.ts
@@ -36,6 +36,14 @@ export class Tag {
   })
   description: string | null;
 
+  @Column({
+    type: 'varchar',
+    length: 50,
+    nullable: true,
+    default: '#3b82f6',
+  })
+  color: string | null;
+
   @ManyToMany(() => Product, (product) => product.tags)
   products: Product[];
 
